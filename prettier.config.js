73c94c0720847e0fa3a2755a4c2e879d6aaa10e7
Thomas Dargon <thomas.dargon@koodoo.io>
2021-05-22 11:38:50 +0100

    Added prettier pug plugin, nodemon, dev script to watch pug files

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+// https://github.com/prettier/plugin-pug
+// https://prettier.github.io/plugin-pug/guide/pug-specific-options.html#empty-attributes
+module.exports = {
+  pugPrintWidth: 80,
+  pugAttributeSeparator: "none",
+  pugClosingBracketPosition: "new-line",
+  pugWrapAttributesThreshold: 2,
+  pugClassNotation: "attribute",
+  pugSortAttributes: "asc",
+  pugIdNotation: "as-is",
+};
