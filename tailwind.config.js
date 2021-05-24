a8014ed087caba80756906f337c408ff664a7021
Thomas Dargon <thomas.dargon@koodoo.io>
2021-05-24 10:57:41 +0100

    Added custom colors through tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    colors: {
+      gray: {
+        50: "#cdd9e5",
+        600: "#495057",
+        800: "#22272e",
+      },
+      blue: {
+        400: "#539bf5",
+      },
+      pink: {
+        600: "#DB277",
+      },
+      transparent: "transparent",
+      white: "#fff",
+    },
+  },
+};
